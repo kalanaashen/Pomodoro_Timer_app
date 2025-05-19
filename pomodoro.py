dffac2f268d68f6318dd2b9a913b26d93336e660
kalanaashen <northernwolf14742@gmail.com>
2025-05-20 00:38:48 +0530

    all done

--- pomodoro.py
+++ pomodoro.py
@@ -1,8 +1,6 @@
 import customtkinter as ctk
 from typing import Tuple
-
 from PIL import Image
-
 ctk.set_appearance_mode("Dark")
 
 class App(ctk.CTk):
@@ -16,71 +14,74 @@ class App(ctk.CTk):
         self.columnconfigure(3,weight=1)
         self.loading_wdigets()
         self.is_working=True
+        self.array=[{"status":"Work","time":1*60},
+                    {"status":"Break","time":1*60},
+                    {"status":"Work","time":25*60},
+                    {"status":"Break","time":5*60},
+                    {"status":"Work","time":25*60},
+                    {"status":"Break","time":5*60},
+                    {"status":"Work","time":25*60},
+                    {"status":"Break","time":30*60}]
+        self.current_index=0
+        self.count=1
+        self.correct_sign_="✔️"
 
 
     def loading_wdigets(self):
-        self.work_break_label=ctk.CTkLabel(self,text="hello world")
+        self.work_break_label=ctk.CTkLabel(self,text="")
         image_tom = Image.open("tomato.png")
         play_pause_image=Image.open("play.png")
         pause_image=Image.open("pause-button.png")
         self.pause_image_last=ctk.CTkImage(light_image=pause_image,dark_image=pause_image,size=(50,50))
         self.play_image=ctk.CTkImage(light_image=play_pause_image,dark_image=play_pause_image,size=(50,50))
         tomato_image=ctk.CTkImage (light_image=image_tom,dark_image=image_tom,size=(400,400))
-
-        self.image_label = ctk.CTkLabel(self, image=tomato_image, text="pakaya", compound="center", font=("Arial", 30, "bold"), text_color="white")
-
-        #timer_label=ctk.CTkLabel(self,text="timer here")
+        self.correct_sign=ctk.CTkLabel(self,text="")
+        self.image_label = ctk.CTkLabel(self, image=tomato_image, text="Press Play!", compound="center", font=("Arial", 30, "bold"), text_color="white")
         self.start_pause_button=ctk.CTkButton(self,image=self.play_image,fg_color="transparent",text="",height=20,width=20,corner_radius=0,border_width=0,hover_color="#1a1a1a",command=self.button_logic)
-        counting_label=ctk.CTkLabel(self,text="Counting label rest here")
-
         self.work_break_label.grid(row=0,column=0)
         self.image_label.grid(row=1,column=0)
-        #timer_label.grid(row=1,column=0)
         self.start_pause_button.grid(row=2,column=0)
-        #counting_label.grid(row=3,column=0)
-
+        self.correct_sign.grid(row=3,column=0)
 
     def button_logic(self):
+
+
         if self.start_pause_button.cget("image")==self.play_image:
-            self.start_pause_button.configure(image=self.pause_image_last)
-            self.work_break_label.configure(text="Work!",font=("Arial",30, "bold"))
-            self.sec_25()
+           
+           self.start_pause_button.configure(image=self.pause_image_last)
+           self.timer()
+            
             
         else:
             self.start_pause_button.configure(image=self.play_image)
             self.image_label.configure(text="00:00",font=("Arial",30, "bold"))
             self.work_break_label.configure(text="You Failed All reseted!",font=("Arial",30, "bold"))
             self.is_working=False
-    def sec_25(self):
-
-        self.is_working=True
-        self.seconds=60
-        
-        self.timer()
-
-
+    
 
     def timer(self):
-      
-        if self.seconds >= 0 and self.is_working :
-            self.rest_minutes = self.seconds // 60
-            self.rest_seconds = self.seconds % 60
-            time_display = f"{self.rest_minutes:02}:{self.rest_seconds:02}"
-            self.image_label.configure(text=time_display, font=("Arial", 30, "bold"))
-            self.seconds -= 1
-            self.after(1000, self.timer)
-        if self.seconds==0:
-            self.seconds=2*60
-            self.is_working=True
+        self.is_working=True
+        status_obj = self.array[self.current_index]
+        self.r_time=status_obj["time"]
+        self.status=status_obj["status"]
+        self.inside_func()
+
+        
+    def inside_func(self):
+        if self.r_time >= 0 and self.is_working :
+                rest_minutes = self.r_time // 60
+                rest_seconds = self.r_time % 60
+                time_display = f"{rest_minutes:02}:{rest_seconds:02}"
+                self.image_label.configure(text=time_display, font=("Arial", 30, "bold"))
+                self.work_break_label.configure(text=self.status,font=("Arial",30, "bold"))
+                self.r_time -= 1
+                self.after(1000, self.inside_func)
+        elif self.is_working and self.current_index < len(self.array):
+            self.current_index+=1
+            if self.status=="Break":
+                self.correct_sign.configure(text=f"{self.correct_sign_*self.count}")
+                self.count+=1
             self.timer()
-            self.work_break_label.configure(text="Break",font=("Arial",30, "bold"))
-
-
-
-    def break_timer(self):
-        self.seconds_break=5*60
-        self.timer()
-
 
 
 
